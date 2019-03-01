65e258dcd3c744e7d4e10bcd9dd4db3f91ce62cb
hiteshsantwani <hiteshsantwani35@gmail.com>
2019-03-01 16:58:15 -0500

    Schema addition

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+// to do: Move configuration to external file
 // mongoose connection
+
+const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'AppointmentScheduler';      // REPLACE WITH YOUR DB NAME
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/AppointmentScheduler', {
-    useMongoClient: true
-});
+mongoose.connect(`mongodb://${server}/${database}`)
+       .then(() => {
+         console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error')
+       });
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,4 +38,3 @@ res.send(`Node and Express Server is running on port ${port}`)
 app.listen(port, () =>
 console.log(`your server is running on port ${port}`)
 );
-
--- src/Models/appointmentSchedulerModels.js
+++ src/Models/appointmentSchedulerModels.js
@@ -25,3 +25,27 @@ export const ContactSchema = new Schema({
        default: Date.now 
     }
 });
+
+export const AppointmentDetailsSchema = new Schema({
+    _id: {
+        type: String,
+        required: 'Enter a first name'
+    },
+    lastName: {
+        type: String,
+        required: 'Enter a last name'
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_date: {
+       type: Date,
+       default: Date.now 
+    }
+});
